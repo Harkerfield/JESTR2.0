84a565db3774a75028171d058fe26f603ea1d3a0
Harkerfield <hartsfieldjwh@gmail.com>
2022-05-08 22:20:34 -0800

    Maps updated

--- src/siteMaps/SiteMaps.js
+++ src/siteMaps/SiteMaps.js
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material';
 
 // openlayers
 import GeoJSON from 'ol/format/GeoJSON'
